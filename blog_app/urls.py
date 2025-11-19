6aa031701af3c63e2f35040c846b696676856be0
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-11-19 11:22:04 +0545

    Configure STATIC and MEDIA settings for serving static and user-uploaded files

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #if request come in /media/ the django will in media folder and return the file
