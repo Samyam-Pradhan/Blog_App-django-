f011b933a62ff956efa45636936d7633ec3de121
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-12-27 14:03:59 +0545

    Updated views and added new templates

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
 from .forms import BlogForm
-from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
+# Redirect index to blog list
 def index(request):
-    return render(request, 'index.html')
+    return redirect('blog_list')
 
-
+# List all blogs (public view)
 def blog_list(request):
     blogs = Blog.objects.all().order_by('-created_at')
-    return render(request, 'blog_list.html', {'blogs': blogs})  
+    return render(request, 'blog_list.html', {'blogs': blogs})
 
-
+# Create a new blog (login required)
+@login_required
 def blog_create(request):
     if request.method == "POST":
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
-            blog = form.save(commit=False)   # don't save to database yet
-            blog.user = request.user         # set logged in user
-            blog.save()                      # now save
+            blog = form.save(commit=False)
+            blog.user = request.user
+            blog.save()
             return redirect('blog_list')
     else:
-        form = BlogForm()                    # create empty form for GET
-        
+        form = BlogForm()
     return render(request, 'blog_form.html', {'form': form})
 
-
+# Edit an existing blog (login required)
+@login_required
 def blog_edit(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id, user=request.user)
-
     if request.method == 'POST':
         form = BlogForm(request.POST, request.FILES, instance=blog)
         if form.is_valid():
@@ -38,15 +39,13 @@ def blog_edit(request, blog_id):
             return redirect('blog_list')
     else:
         form = BlogForm(instance=blog)
-
     return render(request, 'blog_form.html', {'form': form})
 
-
+# Delete a blog (login required)
+@login_required
 def blog_delete(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id, user=request.user)
-
     if request.method == 'POST':
         blog.delete()
-        return redirect('blog_list')   
-    
+        return redirect('blog_list')
     return render(request, 'blog_confirm_delete.html', {'blog': blog})
